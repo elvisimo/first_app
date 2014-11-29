95d4fdbb594fb075bae72887f81fe38e37f7b0bf
silend <solokhinii@gmail.com>
2014-11-29 04:59:26 +0400

    Initialize repository

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '38f2dc866c991cf9b8227ddf2b5a70ed7b6e63264678503f63729987688ba1c17bd7752ba04cf10eeaf082806e26f74ad8d0fd59b4009ca5c1ffbb7ed3dd6118'
---
+++ first_app/server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '371f33f80667d0f46a7be2d6e164e0795e973932f9ec39f1f841e39e086987853449328c6b57b1affbd4a1324b18e0bac4efe590f7be21bcda865f9d09c00f8a';
